db764e3b0605ad9c28c6d1087eb60efdf6d31926
Руслан Корнев <oganer@gmail.com>
2012-11-13 04:37:48 +0400

    For now validation is wrong. I can create owner and fill only second task name. But validation for first task is missing

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,7 +2,7 @@ class Owner < ActiveRecord::Base
   attr_accessible :name
   validates :name, :presence => true
 
-  has_many :tasks
+  has_many :tasks, :inverse_of => :owner
   attr_accessible :tasks_attributes
   accepts_nested_attributes_for :tasks 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,6 @@ class Task < ActiveRecord::Base
   attr_accessible :name
   validates :name, :presence => true
 
-  belongs_to :owner
+  belongs_to :owner, :inverse_of => :tasks, :validate => true
+  validates :owner, :presence => true
 end
