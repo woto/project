db5f8b02de447da2dc0a657e3ea8707d95f27558
Руслан Корнев <oganer@gmail.com>
2012-11-13 04:15:53 +0400

    ./script/rails g scaffold Owner name:string

---
+++ app/models/owner.rb
@@ -0,0 +1,3 @@
+class Owner < ActiveRecord::Base
+  attr_accessible :name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Project::Application.routes.draw do
+  resources :owners
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
